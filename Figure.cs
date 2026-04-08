3bf287376ac0a6c4411d79217f18303b5aafb117
Lakashmak <lakashmak@gmail.com>
2026-04-08 05:24:14 +0500

    V0.7: fixed the displaying

--- Figure.cs
+++ Figure.cs
@@ -17,51 +17,191 @@ namespace растеризатор
 		public List<Quaternion> points;
 		public List<Face> faces;
 		public double r = 2;
+        public int id;
+		Random rnd;
 
-		public Figure(string type, Quaternion location)
+		public Figure(int id, string type, Quaternion location)
 		{
+			this.id = id;
+			rnd = new Random();
 			this.location = new Quaternion(location);
 			points = new List<Quaternion>();
 			faces = new List<Face>();
-			
-			if(type == "Cube")
+			name = type;
+			if(type == "Stellated octahedron")
 			{
-				name = "Cube";
-				for(int i = 0; i < 8; i++) points.Add(new Quaternion(((i/8)%2 - 0.5)*0, ((i/4)%2 - 0.5)*r, ((i/2)%2 - 0.5)*r, (i%2 - 0.5)*r));
-				Update();
+				for (int i = 0; i < 8; i++) points.Add(new Quaternion(((i / 8) % 2 - 0.5) * 0, ((i / 4) % 2 - 0.5) * r, ((i / 2) % 2 - 0.5) * r, (i % 2 - 0.5) * r));
+				points.Add(new Quaternion(0, 0, -r/2));
+				points.Add(new Quaternion(0, -r/2, 0));
+				points.Add(new Quaternion(-r/2, 0, 0));
+				points.Add(new Quaternion(r/2, 0, 0));
+				points.Add(new Quaternion(0, r/2, 0));
+				points.Add(new Quaternion(0, 0, r/2));
+				for (int h1 = 0; h1 < points.Count; h1++) for (int h2 = 0; h2 < points.Count; h2++)
+				{
+					bool i = true;
+					foreach (Face l in faces) if(l.h1 == h2 && l.h2 == h1) i = false;
+					if(i && points[h1].Sub(points[h2]).Abs() == Math.Sqrt(2)*r/2) faces.Add(new Face(id, h1, h2));
+				}
+				int alp = 191;
+				List<int> p;
+				List<Color> cl = new List<Color>(); 
+				for(int i = 0; i < 8; i++) cl.Add(Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256)));
+				p = new List<int>(); p.Add(0); p.Add( 8); p.Add( 9); faces.Add(new Face(id, p, cl[0]));
+				p = new List<int>(); p.Add(0); p.Add( 8); p.Add(10); faces.Add(new Face(id, p, cl[1]));
+				p = new List<int>(); p.Add(0); p.Add( 9); p.Add(10); faces.Add(new Face(id, p, cl[2]));
+				p = new List<int>(); p.Add(1); p.Add(13); p.Add( 9); faces.Add(new Face(id, p, cl[3]));
+				p = new List<int>(); p.Add(1); p.Add(13); p.Add(10); faces.Add(new Face(id, p, cl[4]));
+				p = new List<int>(); p.Add(1); p.Add( 9); p.Add(10); faces.Add(new Face(id, p, cl[5]));
+				p = new List<int>(); p.Add(2); p.Add( 8); p.Add(12); faces.Add(new Face(id, p, cl[6]));
+				p = new List<int>(); p.Add(2); p.Add( 8); p.Add(10); faces.Add(new Face(id, p, cl[5]));
+				p = new List<int>(); p.Add(2); p.Add(12); p.Add(10); faces.Add(new Face(id, p, cl[4]));
+				p = new List<int>(); p.Add(3); p.Add(13); p.Add(12); faces.Add(new Face(id, p, cl[7]));
+				p = new List<int>(); p.Add(3); p.Add(13); p.Add(10); faces.Add(new Face(id, p, cl[2]));
+				p = new List<int>(); p.Add(3); p.Add(12); p.Add(10); faces.Add(new Face(id, p, cl[1]));
+				p = new List<int>(); p.Add(4); p.Add( 8); p.Add( 9); faces.Add(new Face(id, p, cl[5]));
+				p = new List<int>(); p.Add(4); p.Add( 8); p.Add(11); faces.Add(new Face(id, p, cl[6]));
+				p = new List<int>(); p.Add(4); p.Add( 9); p.Add(11); faces.Add(new Face(id, p, cl[3]));
+				p = new List<int>(); p.Add(5); p.Add(13); p.Add( 9); faces.Add(new Face(id, p, cl[2]));
+				p = new List<int>(); p.Add(5); p.Add(13); p.Add(11); faces.Add(new Face(id, p, cl[7]));
+				p = new List<int>(); p.Add(5); p.Add( 9); p.Add(11); faces.Add(new Face(id, p, cl[0]));
+				p = new List<int>(); p.Add(6); p.Add( 8); p.Add(12); faces.Add(new Face(id, p, cl[1]));
+				p = new List<int>(); p.Add(6); p.Add( 8); p.Add(11); faces.Add(new Face(id, p, cl[0]));
+				p = new List<int>(); p.Add(6); p.Add(12); p.Add(11); faces.Add(new Face(id, p, cl[7]));
+				p = new List<int>(); p.Add(7); p.Add(13); p.Add(12); faces.Add(new Face(id, p, cl[4]));
+				p = new List<int>(); p.Add(7); p.Add(13); p.Add(11); faces.Add(new Face(id, p, cl[3]));
+				p = new List<int>(); p.Add(7); p.Add(12); p.Add(11); faces.Add(new Face(id, p, cl[6]));
+			} else
+			if(type == "Tetrahedron")
+			{
+				points.Add(new Quaternion(-r/2, -r/2, -r/2));
+				points.Add(new Quaternion(-r/2, r/2, r/2));
+				points.Add(new Quaternion(r/2, -r/2, r/2));
+				points.Add(new Quaternion(r/2, r/2, -r/2));
+				for (int h1 = 0; h1 < points.Count; h1++) for (int h2 = 0; h2 < points.Count; h2++)
+				{
+					bool i = true;
+					foreach (Face l in faces) if(l.h1 == h2 && l.h2 == h1) i = false;
+					if(i && points[h1].Sub(points[h2]).Abs() == Math.Sqrt(2)*r) faces.Add(new Face(id, h1, h2));
+				}
+				int alp = 191;
+				List<int> p;
+				p = new List<int>(); p.Add(0); p.Add(1); p.Add(2); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(0); p.Add(1); p.Add(3); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(0); p.Add(2); p.Add(3); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(1); p.Add(2); p.Add(3); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+			} else
+			if(type == "Octahedron")
+			{
+				points.Add(new Quaternion(0, 0, -r/2));
+				points.Add(new Quaternion(0, -r/2, 0));
+				points.Add(new Quaternion(-r/2, 0, 0));
+				points.Add(new Quaternion(r/2, 0, 0));
+				points.Add(new Quaternion(0, r/2, 0));
+				points.Add(new Quaternion(0, 0, r/2));
+				for (int h1 = 0; h1 < points.Count; h1++) for (int h2 = 0; h2 < points.Count; h2++)
+				{
+					bool i = true;
+					foreach (Face l in faces) if(l.h1 == h2 && l.h2 == h1) i = false;
+					if(i && points[h1].Sub(points[h2]).Abs() == Math.Sqrt(2)*r/2) faces.Add(new Face(id, h1, h2));
+				}
+				int alp = 191;
+				List<int> p;
+				for (int i = 0; i < 6; i++) for (int j = i+1; j < 6; j++) for (int k = j+1; k < 6; k++)
+				if(i != j && i != 5-j && i != k && i != 5-k && k != j && k != 5-j)
+				{ p = new List<int>(); p.Add(i); p.Add(j); p.Add(k); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256)))); }
+				//p = new List<int>(); p.Add(0); p.Add(1); p.Add(2); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				//p = new List<int>(); p.Add(0); p.Add(1); p.Add(3); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				//p = new List<int>(); p.Add(0); p.Add(2); p.Add(4); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				//p = new List<int>(); p.Add(0); p.Add(3); p.Add(4); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				//p = new List<int>(); p.Add(1); p.Add(2); p.Add(5); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				//p = new List<int>(); p.Add(1); p.Add(3); p.Add(5); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				//p = new List<int>(); p.Add(2); p.Add(4); p.Add(5); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				//p = new List<int>(); p.Add(3); p.Add(4); p.Add(5); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+			} else
+			if(type == "Tesseract")
+			{
+				for(int i = 0; i < 16; i++) points.Add(new Quaternion(((i/8)%2 - 0.5)*r, ((i/4)%2 - 0.5)*r, ((i/2)%2 - 0.5)*r, (i%2 - 0.5)*r));
+				for (int h1 = 0; h1 < points.Count; h1++) for (int h2 = 0; h2 < points.Count; h2++)
+				{
+					bool i = true;
+					foreach (Face l in faces) if(l.h1 == h2 && l.h2 == h1) i = false;
+					if(i && points[h1].Sub(points[h2]).Abs() == r) faces.Add(new Face(id, h1, h2));
+				}
+				int alp = 63;
+				List<int> p;
+				p = new List<int>(); p.Add(0); p.Add(1); p.Add(3); p.Add(2); faces.Add(new Face(id, p, Color.FromArgb(alp,   0, 255, 255)));
+				p = new List<int>(); p.Add(4); p.Add(5); p.Add(7); p.Add(6); faces.Add(new Face(id, p, Color.FromArgb(alp, 255,   0,   0)));
+				p = new List<int>(); p.Add(0); p.Add(1); p.Add(5); p.Add(4); faces.Add(new Face(id, p, Color.FromArgb(alp, 255,   0, 255)));
+				p = new List<int>(); p.Add(1); p.Add(3); p.Add(7); p.Add(5); faces.Add(new Face(id, p, Color.FromArgb(alp,   0,   0, 255)));
+				p = new List<int>(); p.Add(3); p.Add(2); p.Add(6); p.Add(7); faces.Add(new Face(id, p, Color.FromArgb(alp,   0, 255,   0)));
+				p = new List<int>(); p.Add(2); p.Add(0); p.Add(4); p.Add(6); faces.Add(new Face(id, p, Color.FromArgb(alp, 255, 255,   0)));
+				p = new List<int>(); p.Add( 8); p.Add( 9); p.Add(11); p.Add(10); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(12); p.Add(13); p.Add(15); p.Add(14); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add( 8); p.Add( 9); p.Add(13); p.Add(12); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add( 9); p.Add(11); p.Add(15); p.Add(13); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(11); p.Add(10); p.Add(14); p.Add(15); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(10); p.Add( 8); p.Add(12); p.Add(14); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(0); p.Add(1); p.Add( 9); p.Add( 8); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(0); p.Add(2); p.Add(10); p.Add( 8); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(0); p.Add(4); p.Add(12); p.Add( 8); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(1); p.Add(3); p.Add(11); p.Add( 9); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(1); p.Add(5); p.Add(13); p.Add( 9); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(2); p.Add(3); p.Add(11); p.Add(10); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(2); p.Add(6); p.Add(14); p.Add(10); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(3); p.Add(7); p.Add(15); p.Add(11); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(4); p.Add(5); p.Add(13); p.Add(12); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(4); p.Add(6); p.Add(14); p.Add(12); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(5); p.Add(7); p.Add(15); p.Add(13); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+				p = new List<int>(); p.Add(6); p.Add(7); p.Add(15); p.Add(14); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256))));
+			} else
+			if(type == "Hexadecahoron")
+			{
+				points.Add(new Quaternion(0, 0, 0, -r/2));
+				points.Add(new Quaternion(0, 0, -r/2, 0));
+				points.Add(new Quaternion(0, -r/2, 0, 0));
+				points.Add(new Quaternion(-r/2, 0, 0, 0));
+				points.Add(new Quaternion(r/2, 0, 0, 0));
+				points.Add(new Quaternion(0, r/2, 0, 0));
+				points.Add(new Quaternion(0, 0, r/2, 0));
+				points.Add(new Quaternion(0, 0, 0, r/2));
+				for (int h1 = 0; h1 < points.Count; h1++) for (int h2 = 0; h2 < points.Count; h2++)
+				{
+					bool i = true;
+					foreach (Face l in faces) if(l.h1 == h2 && l.h2 == h1) i = false;
+					if(i && points[h1].Sub(points[h2]).Abs() == Math.Sqrt(2)*r/2) faces.Add(new Face(id, h1, h2));
+				}
+				int alp = 63;
+				List<int> p;
+				for (int i = 0; i < 8; i++) for (int j = i+1; j < 8; j++) for (int k = j+1; k < 8; k++)
+				if(i != j && i != 7-j && i != k && i != 7-k && k != j && k != 7-j)
+				{ p = new List<int>(); p.Add(i); p.Add(j); p.Add(k); faces.Add(new Face(id, p, Color.FromArgb(alp, rnd.Next(256), rnd.Next(256), rnd.Next(256)))); }
 			}
 			else
 			{
 				name = "Cube";
 				for (int i = 0; i < 8; i++) points.Add(new Quaternion(((i / 8) % 2 - 0.5) * 0, ((i / 4) % 2 - 0.5) * r, ((i / 2) % 2 - 0.5) * r, (i % 2 - 0.5) * r));
-				Update();
-			}
-		}
-		
-		public void Update()
-		{
-			faces = new List<Face>();
-			if(name == "Cube")
-			{
-				foreach (Quaternion h1 in points) foreach (Quaternion h2 in points)
+				for (int h1 = 0; h1 < points.Count; h1++) for (int h2 = 0; h2 < points.Count; h2++)
 				{
 					bool i = true;
 					foreach (Face l in faces) if(l.h1 == h2 && l.h2 == h1) i = false;
-					if(i && h1.Sub(h2).Abs() == r) faces.Add(new Face(h1, h2));
+					if(i && points[h1].Sub(points[h2]).Abs() == r) faces.Add(new Face(id, h1, h2));
 				}
-				List<Quaternion> p = new List<Quaternion>(); p.Add(points[0]); p.Add(points[1]); p.Add(points[3]); p.Add(points[2]);
-				faces.Add(new Face(p, Color.FromArgb(191, 0, 255, 255)));
-				p = new List<Quaternion>(); p.Add(points[4]); p.Add(points[5]); p.Add(points[7]); p.Add(points[6]);
-				faces.Add(new Face(p, Color.FromArgb(191, 255, 0, 0)));
-				p = new List<Quaternion>(); p.Add(points[0]); p.Add(points[1]); p.Add(points[5]); p.Add(points[4]);
-				faces.Add(new Face(p, Color.FromArgb(191, 255, 0, 255)));
-				p = new List<Quaternion>(); p.Add(points[1]); p.Add(points[3]); p.Add(points[7]); p.Add(points[5]);
-				faces.Add(new Face(p, Color.FromArgb(191, 0, 0, 255)));
-				p = new List<Quaternion>(); p.Add(points[3]); p.Add(points[2]); p.Add(points[6]); p.Add(points[7]);
-				faces.Add(new Face(p, Color.FromArgb(191, 0, 255, 0)));
-				p = new List<Quaternion>(); p.Add(points[2]); p.Add(points[0]); p.Add(points[4]); p.Add(points[6]);
-				faces.Add(new Face(p, Color.FromArgb(191, 255, 255, 0)));
+				int alp = 191;
+				List<int> p;
+				p = new List<int>(); p.Add(0); p.Add(1); p.Add(3); p.Add(2); faces.Add(new Face(id, p, Color.FromArgb(alp,   0, 255, 255)));
+				p = new List<int>(); p.Add(4); p.Add(5); p.Add(7); p.Add(6); faces.Add(new Face(id, p, Color.FromArgb(alp, 255,   0,   0)));
+				p = new List<int>(); p.Add(0); p.Add(1); p.Add(5); p.Add(4); faces.Add(new Face(id, p, Color.FromArgb(alp, 255,   0, 255)));
+				p = new List<int>(); p.Add(1); p.Add(3); p.Add(7); p.Add(5); faces.Add(new Face(id, p, Color.FromArgb(alp,   0,   0, 255)));
+				p = new List<int>(); p.Add(3); p.Add(2); p.Add(6); p.Add(7); faces.Add(new Face(id, p, Color.FromArgb(alp,   0, 255,   0)));
+				p = new List<int>(); p.Add(2); p.Add(0); p.Add(4); p.Add(6); faces.Add(new Face(id, p, Color.FromArgb(alp, 255, 255,   0)));
 			}
+		}
+		
+		void IdUpdate(int id)
+		{
+			this.id = id;
+			foreach (Face f in faces) f.id = id;
 		}
 	}
 }
--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -17,6 +17,7 @@ namespace растеризатор
 		private System.Windows.Forms.PictureBox pictureBox1;
 		private System.Windows.Forms.Timer timer1;
 		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -39,53 +40,70 @@ namespace растеризатор
 		/// </summary>
 		private void InitializeComponent()
 		{
-            this.components = new System.ComponentModel.Container();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(585, 480);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
-            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1MouseMove);
-            // 
-            // timer1
-            // 
-            this.timer1.Interval = 1;
-            this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 3);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
-            // 
-            // MainForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(585, 480);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
-            this.Name = "MainForm";
-            this.Text = "растеризатор";
-            this.SizeChanged += new System.EventHandler(this.MainFormSizeChanged);
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyDown);
-            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(585, 480);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1MouseMove);
+			// 
+			// timer1
+			// 
+			this.timer1.Interval = 1;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 3);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "label1";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(3, 111);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(38, 20);
+			this.numericUpDown1.TabIndex = 2;
+			this.numericUpDown1.TabStop = false;
+			this.numericUpDown1.Value = new decimal(new int[] {
+			2,
+			0,
+			0,
+			0});
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(585, 480);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "MainForm";
+			this.Text = "растеризатор";
+			this.SizeChanged += new System.EventHandler(this.MainFormSizeChanged);
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyDown);
+			this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.MainFormKeyUp);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 	}
